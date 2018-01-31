7ebb227a7c074b4442027266b65a835298d1b27c
X1Warrior <zsto7638@gmail.com>
2018-01-31 11:40:01 -0700

    Made  controller and runner and made the basic layouts for them.

---
+++ DataStructures/Controller/Controller.cpp
@@ -0,0 +1,17 @@
+//
+//  Controller.cpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 1/31/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+using namespace std;
+
+void Controller :: start()
+{
+    
+    
+}
---
+++ DataStructures/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 1/31/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "Controller/Controller.hpp"
+int main()
+{
+    Controller app;
+    app.start();
+    return 0;
+}
