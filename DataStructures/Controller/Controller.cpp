b65d5d02db9b6240691bf06d462fa9cf48506db3
X1Warrior <zsto7638@gmail.com>
2018-02-02 11:48:49 -0700

    created the min and max searcher.

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -12,21 +12,33 @@ using namespace std;
 
 void Controller :: start()
 {
-    Timer codeTimer;
-    codeTimer.startTimer();
-    cout << "Look code on the screen" << endl;
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    codeTimer.resetTimer();
-    codeTimer.startTimer();
+    findMaxAndMin();
+}
 
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/zsto7638/Documents/2420 Data Stuff/DataStructures/DataStructures/Data/crime.csv");
     
-    for (int index = 200; index < 216; index++)
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 1; index < myData.size(); index++)
     {
-        cout << index << " is " << myData[index] << endl;
+       if (myData[minIndex] > myData [index])
+       {
+           minIndex = index;
+       }
+        if (myData [maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
+        
     }
     
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
+    searchTimer.stopTimer();
+    cout << "The smallest Crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The smallest Crime stat is at " << maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
 }
