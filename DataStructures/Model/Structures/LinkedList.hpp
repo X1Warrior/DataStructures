c4bf66514fd19dfd0e9c107b9c8b17977279c0ea
X1Warrior <zsto7638@gmail.com>
2018-02-14 12:27:27 -0700

    Added a Linked list class then am setting up the perameters/List for it.

---
+++ DataStructures/Model/Structures/LinkedList.hpp
@@ -0,0 +1,89 @@
+//
+//  LinkedList.hpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 2/14/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "List.hpp"
+
+using namespace std; //Used for keyword acess. Use this to make sure your pointers are pointing to the right things
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+    
+public:
+    //Constructors
+    LinkedList();
+    //Destructor
+    virtual ~LinkedList(); // Virtual so it can be overridden.
+    //Helper methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    //Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    //Type setAtIndex(int index, Type item);
+    //bool contains(Type item);
+}
+template <class Type>
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
+
+template <class Type>
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure = front;
+    while (front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+}
+template <class Type>
+void LinkedList,Type> :: add(Type item)
+{
+    LinearNode<Type> * newData = new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = newData;
+    }
+    else
+    {
+        this->end->setNextNode(newData);
+    }
+    
+    this->end = newData;
+    
+    this->size += 1;
+}
+
+
+
+
+
+
+
+
+
+    
+    
+#endif /* LinkedList_hpp */
