0583efab630525255461e6eef280a13a7ddc9a5f
X1Warrior <zsto7638@gmail.com>
2018-03-05 11:45:04 -0700

    Added the hpp and stub files for the recursion tester.

---
+++ DataStructures/Testers/RecursionTester.cpp
@@ -0,0 +1,29 @@
+//
+//  RecursionTester.cpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 3/5/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "RecursionTester.hpp"
+
+int RecursionTester :: fib(int word)
+{
+    return 0;
+}
+
+string RecursionTester :: mystery(string word)
+{
+    return 0;
+}
+
+void RecursionTester :: testRecursionNumbers()
+{
+    
+}
+
+void RecursionTester :: testRecursionString()
+{
+    
+}
