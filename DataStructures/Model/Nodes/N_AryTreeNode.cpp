4e3e861ccb5b88e601175c8cd152c6363540d981
X1Warrior <zsto7638@gmail.com>
2018-04-23 11:52:51 -0600

    Added the N_AryTreeFile to go through nodes.

---
+++ DataStructures/Model/Nodes/N_AryTreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  N_AryTreeNode.cpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 4/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "N_AryTreeNode.hpp"
---
+++ DataStructures/Model/Nodes/N_AryTreeNode.hpp
@@ -0,0 +1,103 @@
+//
+//  N_AryTreeNode.hpp
+//  DataStructures
+//
+//  Created by Stout, Zachary on 4/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#ifndef N_AryTreeNode_hpp
+#define N_AryTreeNode_hpp
+
+#include <vector>
+#include "Node.hpp"
+
+template <class Type>
+class N_AryTreeNode : public Node<Type>
+{
+private:
+    vector<N_AryTreeNode<Type>*> nodes;
+    N_AryTreeNode<Type> * root;
+    
+public:
+    N_AryTreeNode<Type>();
+    ~N_AryTreeNode<Type>();
+    N_AryTreeNode<Type>(Type data);
+    
+    vectore<N_AryTreeNode<Type>*> getNodes();
+    N_AryTreeNode<Type> * getRoot();
+    int getChildCount();
+    
+    void setRoot(N_AryTreeNode<Type> * root);
+    void addChild(Type data);
+};
+
+template <class Type>
+class N_AryTreeNode<Type> :: N_AryTreeNode()
+{ //unused
+}
+
+template <class Type>
+class N_AryTreeNode<Type> :: N_AryTreeNode(Type data) : Node<Type>(data)
+{
+}
+
+template <class Type>
+class N_AryTreeNode<Type> :: ~N_ArayTreeNode
+{
+    for (int index = nodes.size() - 1; index >= 0; index --)
+    {
+        delete nodes[index];
+    }
+}
+
+template <class Type>
+N_AryTreeNode<Type> * N_AryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+vector<N_AryTreeNode<Type>*> N_AryTreeNode<Type> :: getNodes()
+{
+    return nodes;
+}
+
+template <class Type>
+int N_AryTreeNode<Type> :: getChildCount()
+{
+    return nodes.size();
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: setRoot(N_AryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: addChild(Type data)
+{
+    N_AryTreeNode<Type> * node = new N_aryTreeNode<Type>(data);
+    nodes.push_back(node);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* N_AryTreeNode_hpp */
